ab2f09c5056bf01e3f9eacf5e0bf7b7da1a0d019
gilzellner <gil.zellner@gmail.com>
2016-02-10 15:27:05 +0200

    CFY-4853-Convert-all-manager-scripts-from-bash-to-python

--- components/manager/scripts/vcloud/update-vcloud-vm.sh
+++ components/manager/scripts/vcloud/update-vcloud-vm.sh
@@ -29,3 +29,20 @@ if [ "z$status" == 'zactive' ]; then
 else
     echo "Skipping update firewall, please update rules manually"
 fi
+
+sudo yum install python-devel gcc-c++ gcc libxslt-devel libxml2-devel python-argparse zlib-devel -y
+
+# add http(s) rules
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+# add influxdb connection
+sudo firewall-cmd --zone=public --add-port=8086/tcp --permanent
+# port for agent download
+sudo firewall-cmd --zone=public --add-port=53229/tcp --permanent
+# port for AQMP
+sudo firewall-cmd --zone=public --add-port=5672/tcp --permanent
+# port for diamond
+sudo firewall-cmd --zone=public --add-port=8101/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8100/tcp --permanent
+
+sudo firewall-cmd --reload
--- components/manager/scripts/vsphere/update-firewall.sh
+++ components/manager/scripts/vsphere/update-firewall.sh
@@ -2,6 +2,7 @@
 
 uname -a
 
+<<<<<<< HEAD
 status=`systemctl status firewalld | grep "Active:"| awk '{print $2}'`
 
 if [ "z$status" == 'zactive' ]; then
@@ -23,3 +24,19 @@ if [ "z$status" == 'zactive' ]; then
 else
     echo "Skipping update firewall, please update rules manually"
 fi
+=======
+# add http(s) rules
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+# add influxdb connection
+sudo firewall-cmd --zone=public --add-port=8086/tcp --permanent
+# port for agent download
+sudo firewall-cmd --zone=public --add-port=53229/tcp --permanent
+# port for AQMP
+sudo firewall-cmd --zone=public --add-port=5672/tcp --permanent
+# port for diamond
+sudo firewall-cmd --zone=public --add-port=8101/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8100/tcp --permanent
+
+sudo firewall-cmd --reload
+>>>>>>> CFY-4853-Convert-all-manager-scripts-from-bash-to-python
